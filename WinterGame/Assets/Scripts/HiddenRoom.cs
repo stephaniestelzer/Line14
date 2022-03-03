e7ada9237b59675c1a09a09e125ec0cbaf8276b8
chewitt1 <44040034+chewitt1@users.noreply.github.com>
2022-03-03 14:32:44 -0500

    Carrima's Changes
    -- Player now changes position and automatically slides/slips on all slope objects
    -- Extended the level (added two more checkpoints worth of platforms/floors/slopes, enemies, collectables: snowflake total is now 1000
    -- Added Hidden Room feature (I got help from the OnCollision code)
    -- Separated the main camera from the player (We can tweak this later, I used and credited code I found on a Unity forum)
    -- Organized the assets in folder groups (the Slopes and Hidden object categories are important because the scripts use them to keep track of asset-types).

---
+++ WinterGame/Assets/Scripts/HiddenRoom.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//TO DO: Add Upper Bound
+public class HiddenRoom : MonoBehaviour//Modelled after OnCollision
+{
+    public Transform poppi;
+    void LateUpdate()
+    {
+        if (poppi.position.z >= (transform.position.z  -1))//-1 to disappear before you get there
+        {
+            if(poppi.position.y < 10)
+            {
+                Debug.Log("Room Unlocked");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
