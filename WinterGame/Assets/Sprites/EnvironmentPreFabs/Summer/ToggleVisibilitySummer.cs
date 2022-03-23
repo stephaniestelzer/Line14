87b4a14f7faad2ea00a3c6dae60a99c6e09c6fa5
Stephanie Stelzer <stephaniestelzer@Stephanies-MacBook-Pro-2.local>
2022-03-23 11:30:02 -0400

    environmentchanges

---
+++ WinterGame/Assets/Sprites/EnvironmentPreFabs/Summer/ToggleVisibilitySummer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleVisibility : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TemperatureManager.temperature == 15){
+            rend.enabled = false;
+        }
+    }
+}
