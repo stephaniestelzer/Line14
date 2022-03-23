55b9ec14f876910892517732c593c30959f5affd
Stephanie Stelzer <stephaniestelzer@Stephanies-MacBook-Pro-2.local>
2022-03-23 18:28:43 -0400

    summer to fall environment platform changes

--- WinterGame/Assets/Sprites/EnvironmentPreFabs/Fall/ToggleVisibilityFall.cs
+++ WinterGame/Assets/Sprites/EnvironmentPreFabs/Fall/ToggleVisibilityFall.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ToggleVisibilityWinter : MonoBehaviour
+public class ToggleVisibilityFall : MonoBehaviour
 {
      public Renderer rend;
     void Start()
--- WinterGame/Assets/Sprites/EnvironmentPreFabs/Summer/ToggleVisibilitySummer.cs
+++ WinterGame/Assets/Sprites/EnvironmentPreFabs/Summer/ToggleVisibilitySummer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ToggleVisibility : MonoBehaviour
+public class ToggleVisibilitySummer : MonoBehaviour
 {
     // Start is called before the first frame update
     public Renderer rend;
