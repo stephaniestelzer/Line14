998099b003dd9127a1559b2430b8a9c4822fb1fa
Andrea Relova <andreamrelova@gmail.com>
2022-03-24 16:34:47 -0400

    Added enemy health bars

--- WinterGame/Assets/Scripts/EnemyStats.cs
+++ WinterGame/Assets/Scripts/EnemyStats.cs
@@ -1,27 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SnazzlebotTools.ENPCHealthBars;
 
 public class EnemyStats : MonoBehaviour
 {
 
     public int maxHealth;
     private int currentHealth;
+    ENPCHealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar = gameObject.GetComponent<ENPCHealthBar>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void takeDamage(int damageAmount) {
         currentHealth -= damageAmount;
+        if (currentHealth < 0)
+          healthBar.Value = 0;
+        else
+          healthBar.Value -= damageAmount;
         if (currentHealth <= 0) { // Enemy is dead
             Destroy(gameObject);
         }
