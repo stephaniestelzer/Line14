9c9a2aeb03135bbf0bd9b64e770816df5638d856
chewitt1 <44040034+chewitt1@users.noreply.github.com>
2022-04-20 14:00:19 -0400

    Horizontal Platforms

--- WinterGame/Assets/Scripts/HorizontalMovingPlatform.cs
+++ WinterGame/Assets/Scripts/HorizontalMovingPlatform.cs
@@ -7,10 +7,30 @@ public class HorizontalMovingPlatform : MonoBehaviour
     bool right = false;
     public int min;
     public int max;
-    float speed = 2f;
+    float speed = 2.5f;
+    public GameObject poppi;
+    BoxCollider bC;
+    bool parent = false;
+
+    void Start()
+    {
+        bC = gameObject.GetComponent<BoxCollider>();
+    }
 
     void FixedUpdate()
     {
+        if ((poppi.transform.position.z >= (bC.bounds.min.z - 1)) && (poppi.transform.position.z <= (bC.bounds.max.z + 1)))
+        {
+            poppi.transform.SetParent(transform);
+            parent = true;
+        }
+        else if (parent)
+        {
+            Debug.Log("CHECK");
+            poppi.transform.SetParent(null);
+            parent = false;
+        }
+
         if (!right)
         {
             if (transform.position.z >= min)
